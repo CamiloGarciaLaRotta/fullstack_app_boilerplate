963ff38a6fdae01eb9537f0d05f6aa4c44396510
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-11-11 22:53:29 -0500

    Added logger

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import sys
+import logging
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
