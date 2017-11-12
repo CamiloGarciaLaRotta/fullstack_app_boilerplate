30a60321bfa1e5e371270f9c78f65500543af336
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-11-12 15:38:12 -0500

    Attempt to fix timedout PORT conn

--- dummy_app/server/server.py
+++ dummy_app/server/server.py
@@ -18,4 +18,5 @@ def index():
 
 if __name__ == '__main__':
     app.debug = APP_STAGE == 'DEVELOPMENT'
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
