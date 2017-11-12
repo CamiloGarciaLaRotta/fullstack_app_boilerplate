b4a1c8630fe4ae7ce5aa19fae40016d2472b76e0
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-11-11 23:24:19 -0500

    Added ENV vars

--- dummy.py
+++ dummy.py
@@ -1,14 +1,15 @@
-from flask import Flask
-import sys
+import sys, os
 import logging
+from flask import Flask
 app = Flask(__name__)
+
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
-    return 'Nepa'
+    return 'Nepe from {}'.format(os.environ['APP_STAGE'])
 
 if __name__ == '__main__':
     app.run()
